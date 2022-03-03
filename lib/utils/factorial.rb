9818a7283fa929f767c7062697df5cd0436774f4
Earl Gresh <earl.gresh@gmail.com>
2022-03-03 14:31:21 -0800

    Update factorial in Utils module
    Added recursive factorial method
    Renamed original factorial method to 'factorial_iterative'
    Added Benchmarks reports for each method when called as a script

--- lib/utils/factorial.rb
+++ lib/utils/factorial.rb
@@ -1,9 +1,26 @@
 module Utils
-  def factorial(num)
+  def factorial_iterative(num)
     sum = 1
     1.upto(num) do |n|
       sum *= n
     end
     sum
+  end
+
+  def factorial_recursive(num)
+    return 1 if num == 1
+
+    num * factorial_recursive(num - 1)
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  include Utils
+  require 'benchmark'
+
+  Benchmark.benchmark(Benchmark::CAPTION, 20, Benchmark::FORMAT, ">total:", ">avg:") do |x|
+    it = x.report('Iterative') { factorial_iterative(100) }
+    re = x.report('Recursive') { factorial_recursive(100) }
+    [re + it, (re + it) / 2]
   end
 end
